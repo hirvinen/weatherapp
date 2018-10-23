c7e505a2b27fc0f9028b0ff7e24d3f8cb9e0354c
Patrik Hirvinen <patrik.hirvinen@iki.fi>
2018-10-23 18:58:20 +0300

    Obey eslint on frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom';
 const baseURL = process.env.ENDPOINT;
 
 // Default to forecast. Current weather is the only currently supported alternative.
-const getWeatherFromApi = async (type = "forecast") => {
+const getWeatherFromApi = async (type = 'forecast') => {
   try {
-    if (type !== "forecast" && type !== "current") throw Error(`Unsupported weather type '${type}'`);
-    const apiPath = type === "current"
-      ? "weather"
+    if (type !== 'forecast' && type !== 'current') throw Error(`Unsupported weather type '${type}'`);
+    const apiPath = type === 'current'
+      ? 'weather'
       : type;
     const response = await fetch(`${baseURL}/${apiPath}`);
     return response.json();
   } catch (error) {
-    console.error(error);
+    console.error(error); // eslint-disable-line no-console
   }
 
   return {};
@@ -24,15 +24,18 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      icon: "",
+      icon: '',
     };
   }
 
   async componentWillMount() {
-    const weatherType = window.location.search.replace(/^\?/, '') || "forecast";
-    const weather     = await getWeatherFromApi(weatherType);
+    // eslint-disable-next-line no-undef
+    const weatherTypeFromSearch = typeof window !== 'undefined' ? window.location.search.replace(/^\?/, '') : '';
+    const weatherType = weatherTypeFromSearch || 'forecast';
 
-    this.setState({icon: weather.icon.slice(0, -1)});
+    const weather = await getWeatherFromApi(weatherType);
+
+    this.setState({ icon: weather.icon.slice(0, -1) });
   }
 
   render() {
@@ -40,7 +43,7 @@ class Weather extends React.Component {
 
     return (
       <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} /> }
+        { icon && <img src={`/img/${icon}.svg`} alt="An icon depicting the type of weather" /> }
       </div>
     );
   }
