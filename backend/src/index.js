2eca6b652ba28635a1404f1a1a7bc00ddd9b47c7
Patrik Hirvinen <patrik.hirvinen@iki.fi>
2018-10-23 19:13:23 +0300

    Obey eslint on the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,11 @@
-const debug = require('debug')('weathermap');
-
 const Koa = require('koa');
 const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
 const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -15,30 +13,30 @@ const app = new Koa();
 
 app.use(cors());
 
-const fetchWeather = async (apiPath = "forecast") => {
+const fetchWeather = async (apiPath = 'forecast') => {
   const endpoint = `${mapURI}/${apiPath}?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
-  const weatherData = await fetchWeather("weather");
+  const weatherData = await fetchWeather('weather');
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
 });
 
 router.get('/api/forecast', async ctx => {
-  const weatherData = await fetchWeather("forecast");
+  const weatherData = await fetchWeather('forecast');
 
   // Get the first forecast that is at least two hours in the future
-  const minimumTime = Math.floor( Date.now() / 1000) + 2 * 60 * 60;
+  const minimumTime = Math.floor(Date.now() / 1000) + 2 * 60 * 60;
   // Guard against data not being ordered in ascending time order
   const maximumTime = minimumTime + 3 * 60 * 60;
 
   const forecast = weatherData.list
-    ? weatherData.list.find( weatherSlice => minimumTime <= weatherSlice.dt && weatherSlice.dt <= maximumTime )
-    : false
+    ? weatherData.list.find(weatherSlice => minimumTime <= weatherSlice.dt && weatherSlice.dt <= maximumTime)
+    : false;
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = forecast
